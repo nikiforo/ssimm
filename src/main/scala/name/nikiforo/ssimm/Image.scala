c38ecc2fddd842d5dcaa0b631e3b308774f89061
nikiforo <me@nikiforo.name>
2017-09-21 17:26:38 +0300

    Add ways for image creation.
    Images can be constructed not only with Paths, but also with java streams.

--- src/main/scala/name/nikiforo/ssimm/Image.scala
+++ src/main/scala/name/nikiforo/ssimm/Image.scala
@@ -1,6 +1,7 @@
 package name.nikiforo.ssimm
 
 import java.awt.image.BufferedImage
+import java.io.{InputStream, OutputStream}
 import java.nio.file.{Files, Path}
 import javax.imageio.ImageIO
 
@@ -11,14 +12,18 @@ trait Image {
   def apply(x: Int, y: Int): Pixel
   def applyFilter(filter: Filter)(implicit resultContainer: ResultContainer) = filter.apply(this)
 
-  def to(path: Path, format: OutputFormat): Unit = {
-    val os = Files.newOutputStream(path)
+  def to(os: OutputStream, format: OutputFormat): Unit = {
     val bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
     for {
       i <- 0 until width
       j <- 0 until height
     } bi.setRGB(i, j, apply(i, j))
     ImageIO.write(bi, format.name, os)
+  }
+
+  def to(path: Path, format: OutputFormat): Unit = {
+    val os = Files.newOutputStream(path)
+    to(os, format)
     os.close()
   }
 }
@@ -26,10 +31,12 @@ trait Image {
 object Image {
   def from(path: Path): Image = {
     val is = Files.newInputStream(path)
-    val img = ImageIO.read(is).toImg
+    val img = from(is)
     is.close()
     img
   }
+
+  def from(is: InputStream): Image = ImageIO.read(is).toImg
 
   def zerosMutable(width: Int, height: Int)(implicit resultContainer: ResultContainer): MutableImage = resultContainer(width, height)
 }
